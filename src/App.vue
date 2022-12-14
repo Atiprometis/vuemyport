1b7f8f6178b1aa7a6442584b5075787aab5c884c
Fang <fang.live-@msn.com>
2022-12-14 16:25:41 +0700

    banner f1

--- src/App.vue
+++ src/App.vue
@@ -40,5 +40,7 @@ export default {
   text-align: center;
   color: #2c3e50;
 padding-top: 55px;
+background-color: #2e6c65;
 }
+
 </style>
--- src/components/banner/BigBlock.vue
+++ src/components/banner/BigBlock.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="con d-flex justify-content-end ">
-        <div class="bigblock-arrow ">
+        <div class="img-b1 d-flex flex-column align-items-center justify-content-center">
+               <h1>I am a Front-end</h1>
+               <h2>Web Developer</h2>
+               <p class="aboutmeskill">
+                HTML,CSS,JACASCRIPT
+               </p>
+                <img class="b1" :src="require(`../../img/b1.png`)" alt="">
+        </div>
+        <div class="bigblock ">
+            
+        </div>
         
-    </div>
-    
-    <!-- <div class="arrow-left"></div> -->
-        <div class="bigblock "></div>
         
-
-    <!-- <div class="bigblock3 ">
         
-    </div> -->
     </div>
 </template>
 
@@ -27,58 +30,25 @@ export default{
 .bigblock{
 
     position: relative;
-    width:400px;
+    width:100%;
     height: 400px;
-    background: #FFEAA7;
-
+    background: #f4c152;
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 59% 100%);
 
 }
-.bigblock-arrow{
+.img-b1{
     position: relative;
-    width: 15%;
-    padding-bottom:15%;
-    /* background: #2a9a28; */
-    transform-origin:100% 0;
-    transform: rotate(-45deg);
-    border-radius: 50px 0px 0px 0;
-    overflow: hidden;
+    width: 100%;
+    height: 400px;
+    background-color: #2e6c65;
+    color: #fff;;
 }
-.bigblock-arrow::after{
+.b1{
+    z-index: 1;
     position: absolute;
-    content: "";
-    top: -1px; right:-3px;
-    width:100%; height:100%;
-    transform: rotate(45deg);
-    background: #FFEAA7;
-    transform-origin:inherit;
-}
-
-.arrow-left {  
-  position: relative;
-  width: 15%;
-  padding-bottom:15%;
-  border-radius: 10px;
-  /* overflow: hidden; */
-  transform-origin:100% 0;
-  /* transform: rotate(-45deg); */
-  background: #FFEAA7;
-}
-.arrow-left:after {
-  content: "";
-  position: absolute;
-  top: -1px; right:-4px;
-  width:100%; height:141%;
-  /* transform-origin:inherit; */
-  /* transform: rotate(45deg); */
-  /* background: #000; */
-  
-}
-.bigblock3{
-
-
-width: 40%;
-height: 400px;
-background: #25318e;
-
+    top: 0;
+    right: -60%;
+    width: 500px;
+    height: 400px;
 }
 </style>
