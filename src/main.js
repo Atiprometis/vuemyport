6538428038e09de1cc5f1571605a33d99a3b7e8a
Fang <fang.live-@msn.com>
2023-04-04 20:56:42 +0700

    add font awesome

--- src/main.js
+++ src/main.js
@@ -6,9 +6,23 @@ import router from '@/router'
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret,faFacebook)
+
 
 createApp(App)
 
 .use(router)
 .use(bootstrap)
+.component("font-awesome-icon", FontAwesomeIcon)
 .mount('#app')
