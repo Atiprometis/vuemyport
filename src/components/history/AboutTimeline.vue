dfdcbfce5d1596e0e1a19e538708a04330fb3b51
Fang <fang.live-@msn.com>
2024-08-19 11:27:17 +0700

    get api EXPERIENCE and EDUCATION display on page

--- src/components/history/AboutTimeline.vue
+++ src/components/history/AboutTimeline.vue
@@ -8,19 +8,21 @@
                 <div class=" col-lg-12 d-flex flex-column ">
                     <div class="col-lg-6 col-sm-12 mt-3  d-flex justify-content-start flex-column text-start ">
                         <h1 class=" text-uppercase"> experience</h1>
-                        <div v-for="project in projects" :key="project.id">
+                        <div v-for="project in projects" :key="project.id_exp">
                             <div   class=" d-flex flex-row ">
                             <div class="timeline"></div>
                             <div class="dot m-1"></div>
                            
                             <div class=" d-flex flex-column text-start ">
-                                <h3 class=" text-uppercase mt-1"> {{ project.name }}</h3>
-                                <p>-</p>
-                                <p class="text-capitalize">{{ project.contect}}</p>
+                                <h3 class=" text-uppercase mt-1"> {{ project.projectname }} | {{ project.location }}</h3>
+                                <!-- <p>-</p> -->
+                                <p class="text-capitalize">{{ project.content}}</p>
                             </div>
                             
                             </div>
                         </div>
+
+                       
                         
 
                         <div class="dot m-1"></div>
@@ -29,15 +31,17 @@
                        
                         <h1 class=" text-uppercase">education</h1>
                         
-                        <div v-for="education in educations" :key="education.id">
+                        <div v-for="education in educations" :key="education.id_edu">
                             <div   class=" d-flex flex-row">
                             <div class="timeline"></div>
                             <div class="dot m-1"></div>
                            
                             <div class=" d-flex flex-column text-start ">
-                                <h3 class=" text-uppercase mt-1"> {{ education.name }}</h3>
-                                <p class="m-0 p-0">{{ education.time}}</p>
-                                <p class="m-0 pb-4 text-capitalize">{{ education.major}}</p>
+                                <h3 class=" text-uppercase mt-1"> {{ education.edu_name }}</h3>
+                                <!-- <p class="m-0 p-0">{{ education.time}}</p> -->
+                                <p class="m-0 pb-4 text-capitalize">{{ education.edu_content}}</p>
+
+
                             </div>
                             
                             </div>
@@ -54,17 +58,45 @@
 </template>
 
 <script>
+import axios from 'axios'
+
     export default{
         name: 'AboutTimeline',
 
         data(){
         return{
            projects:[
-            {id:0,name:"thesis project  | ilovetraining",contect:"this about website promote the course manage the course and make a resume using laravel and mysql"},
-           {id:1,name:"internship | Appsynth company",contect:"I have interned at Appsynth cooperation for 3 months.I learned mobile application development via React Native with Firebase"}
+        //     {id:0,name:"thesis project  | ilovetraining",contect:"this about website promote the course manage the course and make a resume using laravel and mysql"},
+        //    {id:1,name:"internship | Appsynth company",contect:"I have interned at Appsynth cooperation for 3 months.I learned mobile application development via React Native with Firebase"}
         ],
-        educations:[{id:0,name:"Silpakorn University",major:"Web and Interactive Media Design",time:"2013"},{id:0,name:"saohai wimolwitthayanukul school",major:"student",time:"2013"}],
+        educations:[
+            
+        ],
         }
+    },
+    methods:{
+        async getExp(){
+            try{
+                const response = await axios.get('http://localhost:3000/readexp')
+               this.projects = response.data
+               console.log('project = '+ this.projects)
+            } catch(error){
+                return console.error('Error fetching quotes:', error);
+            }
+        },
+        async getEducation(){
+            try{
+                const response = await axios.get('http://localhost:3000/readeducation')
+               this.educations = response.data
+               console.log('project = '+ this.projects)
+            } catch(error){
+                return console.error('Error fetching quotes:', error);
+            }
+        }
+    },
+    beforeMount(){
+        this.getExp();
+        this.getEducation();
     }
     }
     
