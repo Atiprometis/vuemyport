f632c39ac62ed1f64a8ecccd7851096b7cab4507
Fang <fang.live-@msn.com>
2024-09-19 11:54:49 +0700

    add upload image

--- src/components/admin/CardAdmin.vue
+++ src/components/admin/CardAdmin.vue
@@ -2,6 +2,7 @@
     <div>
         
         <slot name="card-header"></slot>
+        
         <slot name="card-content"></slot>
         <slot name="card-button"></slot>
     </div>
--- src/components/admin/component/PortfolioAll.vue
+++ src/components/admin/component/PortfolioAll.vue
@@ -3,10 +3,25 @@
                 <div  class="bg-t-2"></div>
                 <CardAdmin>
                     <template v-slot:card-header>
-                        <h1>พอร์ต</h1>
+                        <h1>portfolio</h1>
                     </template>
                     <template v-slot:card-content >
-                        <h1>ดดด</h1>
+                        <h1>ชื่อโปรเจค</h1>
+                        <form v-on:submit="nameproject" class="form-label">
+                            <input type="text" v-model="insertEduName" class="form-control " placeholder="โปรเจค">
+                            <h1>image อัพโหลด</h1>
+                            <input type="file"  class="form-control " @change="onFileUploadImages" placeholder="เนื้อหา">
+                            
+                       
+                            <button  type="submit"  class="btn btn-primary">เพิ่ม</button>
+
+                        </form>
+                        <img v-if="imageUrlPortfolioUpload" :src="imageUrlPortfolioUpload" alt="Preview" style=" height: 100px;width: 100px;"/>
+                        <h1>img : {{ imageUrlPortfolioUpload }}</h1>
+                        <h1>type</h1>
+                        <h1>อธิบายโปรเจค</h1>
+
+
                     </template>
                     <template v-slot:card-button >
                         
@@ -25,12 +40,28 @@ import CardAdmin from '../CardAdmin.vue'
 
 export default {
   name: 'PortfolioAll',
+  data() {
+    return {
+      imageUrlPortfolioUpload: null,
+    };
+  },
   components: {
     CardAdmin,
     
   },
   methods:{
+    onFileUploadImages(event){
+        event.preventDefault();
+        const file = event.target.files[0];
 
+        if(file){
+            const render = new FileReader();
+            render.onload = (e) => {
+                this.imageUrlPortfolioUpload = e.target.result;
+            }
+            render.readAsDataURL(file);
+        }
+    },
 
   
   }
